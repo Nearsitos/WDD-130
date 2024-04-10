67703ae3adfa5aa7602540a02179bf985aac8889
Nearsitos <chris.rubio.cheno@gmail.com>
2024-04-09 23:48:45 -0700

    Almost Done Personal Website

---
+++ NearsitosPC/JavaScript/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Function to toggle the social menu
+function toggleSocialMenu() {
+    console.log("Toggle social menu called"); // Add a log statement to check if the function is called
+    var menu = document.getElementById("socialMenu");
+    menu.classList.toggle("show");
+}
